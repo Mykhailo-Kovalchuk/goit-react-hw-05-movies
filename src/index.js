e203fd9973393ddc5ba79c1c6959cf51b704d793
Mykhailo-Kovalchuk <contact.kovalchuk@gmail.com>
2024-01-20 17:20:07 +0100

    goit-react-hw-05-movies-commit 10

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter > 
+    <BrowserRouter basename='/'> 
     <App />
     </BrowserRouter> 
   </React.StrictMode>
 );
-// basename='/goit-react-hw-05-movies'
